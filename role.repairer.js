a403c8e0f801d40c1e137e017192d5f7abf060ec
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-08 13:40:21 +0000

    Now repairs the most decayed structures first

--- role.repairer.js
+++ role.repairer.js
@@ -1,3 +1,5 @@
+// Importing modules
+require('prototype.creep')();
 let roleBuilder = require('role.builder');
 
 let roleRepairer = {
@@ -16,13 +18,25 @@ let roleRepairer = {
 
         // Repair the first structure in the list
         if(creep.memory.repairing) {
+            // Creating an array of all structures that need repairs
             let targets = creep.room.find(FIND_STRUCTURES, {
-                filter: object => object.hits < object.hitsMax
+                filter: object => object.hits < (object.hitsMax)
                 && object.structureType != STRUCTURE_WALL
             });
+
+            // Sorting the structures from least decayed to most decayed
+            targets = targets.sort(function(a,b) {
+                return b.hits - a.hits
+            });
+            
+            // DEBUG: logging all targets
+            /*for(let i = 0; i < targets.length; i++) {
+                console.log(targets[i].hits, i)
+            }*/
+
             if(targets.length) {
-                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {reusePath: 3}, {visualizePathStyle: {stroke: '#ff0000'}});
+                if(creep.repair(targets[targets.length - 1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[targets.length - 1], {reusePath: 3}, {visualizePathStyle: {stroke: '#ff0000'}});
                 }
             }
             // If there's nothing to repair, switch to builder
@@ -32,10 +46,7 @@ let roleRepairer = {
         }
         // Refill from energy source
         else {
-            let sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {reusePath: 3}, {visualizePathStyle: {stroke: '#ff7373'}});
-            }
+            creep.refill();
         }
     }
 };
