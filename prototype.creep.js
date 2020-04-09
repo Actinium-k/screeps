2f7e5e453126b6b6bf0ee0b0b04a9e5997812170
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-09 10:04:27 +0000

    Fixed withdrawing from tombstones

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,27 +1,44 @@
 module.exports = function() {
 
-// Refill from energy source
-Creep.prototype.refill = function() {
-    // Create an array of non-empty tombstones, dropped energy and active sources
-    let sources = this.room.find(FIND_SOURCES_ACTIVE);
-    
-    // Find the closest source of energy
-    let target = this.pos.findClosestByPath(sources);
-    
-    if(this.harvest(target) == ERR_NOT_IN_RANGE) {
-            return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
-    }
-    else {
-        /*// DEBUG: nothing should end up here
-        console.log('------')
-        console.log('Creep name', this.name)
-        console.log('Creep target', target)
-        //this.say('Bug')*/
-    }
-};
+    /*// Refill from energy source
+    Creep.prototype.refill = function() {
+        // Create an array of non-empty tombstones, dropped energy and active sources
+        let sources = this.room.find(FIND_SOURCES_ACTIVE);
+        
+        // Find the closest source of energy
+        let target = this.pos.findClosestByPath(sources);
+        
+        if(this.harvest(target) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    };*/
 
-    // Debug function
+    // Refill from energy source
+    Creep.prototype.refill = function() {
+        // Create an array of non-empty tombstones and active sources
+        let sources = (this.room.find(FIND_TOMBSTONES, {
+            filter: (t) => t.store[RESOURCE_ENERGY] > 0
+        })).concat(this.room.find(FIND_SOURCES_ACTIVE));
+        
+        // Find the closest source of energy
+        let target = this.pos.findClosestByPath(sources);
+        
+        // If the target is a tombstone, withdraw from it
+        if(target instanceof Tombstone && target.store[RESOURCE_ENERGY] > 0) {
+            console.log(target.store[RESOURCE_ENERGY])
+            this.say('t = tomb')
+            if(this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        // Else, the target is a source so we harvest it
+        else if(this.harvest(target) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    };
+
+    /*// Debug function
     Creep.prototype.sayHello = function() {
         return this.say('Hello!');
-    };
+    };*/
 }
