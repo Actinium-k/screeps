bc044e7a58b6ead9ba64998f5e23e9eb3851094d
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-09 11:13:16 +0000

    Minor fixes

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,18 +1,5 @@
 module.exports = function() {
 
-    /*// Refill from energy source
-    Creep.prototype.refill = function() {
-        // Create an array of non-empty tombstones, dropped energy and active sources
-        let sources = this.room.find(FIND_SOURCES_ACTIVE);
-        
-        // Find the closest source of energy
-        let target = this.pos.findClosestByPath(sources);
-        
-        if(this.harvest(target) == ERR_NOT_IN_RANGE) {
-                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
-        }
-    };*/
-
     // Refill from energy source
     Creep.prototype.refill = function() {
         // Create an array of non-empty tombstones and active sources
@@ -25,8 +12,6 @@ module.exports = function() {
         
         // If the target is a tombstone, withdraw from it
         if(target instanceof Tombstone && target.store[RESOURCE_ENERGY] > 0) {
-            console.log(target.store[RESOURCE_ENERGY])
-            this.say('t = tomb')
             if(this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
             }
