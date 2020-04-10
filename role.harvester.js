5703b623e5aa87c1457f80b9155196c42480a855
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-10 07:31:49 +0000

    Fixed harvesters not using more than one CARRY

--- role.harvester.js
+++ role.harvester.js
@@ -3,35 +3,49 @@ require('prototype.creep')();
 
 let roleHarvester = {
 
-    /** @param {Creep} creep **/
     run: function(creep) {
 
+        // If the creep is working and has less than 50 energy, stop working
+        if (creep.memory.working == true && creep.store.getUsedCapacity() < 50) {
+            creep.memory.working = false;
+        }
+        // Once the creep is full, switch to working state
+        else if (creep.memory.working == false && creep.store.getUsedCapacity() == creep.store.getCapacity()) {
+            creep.memory.working = true;
+        }
+
         // Find an energy source to harvest from
-        if(creep.store.getFreeCapacity() > 0) {
+        if(creep.memory.working == false) {
             creep.refill();
         }
 
         // Refill existing structures with energy
-        else {
+        else if(creep.memory.working) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (s) => {
                     return (s.structureType == STRUCTURE_EXTENSION ||
                         s.structureType == STRUCTURE_SPAWN ||
-                        s.structureType == STRUCTURE_CONTAINER ||
                         s.structureType == STRUCTURE_TOWER) &&
                         s.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
+
+            // Find closest target
+            var target = creep.pos.findClosestByPath(targets);
+
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {reusePath: 3}, {visualizePathStyle: {stroke: '#ffffff'}});
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {reusePath: 3}, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
             // Move to a parking area if the structures are full
-            else if((creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_FULL)
-                    || (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_INVALID_TARGET)) {
+            else if((creep.transfer(target, RESOURCE_ENERGY) == ERR_FULL)
+                    || (creep.transfer(target, RESOURCE_ENERGY) == ERR_INVALID_TARGET)) {
                 creep.moveTo(Game.flags['parking_area_W36S38'])
             }
+        }
+        else {
+            creep.say('ERROR')
         }
     }
 };
