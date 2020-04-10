fdfea0a1c788af4a3834c5abeff479c583d5c889
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-10 14:38:20 +0000

    Optimized towers code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//require('prototype.spawn')();
-//require('prototype.creep')();
+// Importing modules
+require('prototype.spawn')();
 let roleHarvester = require('role.harvester');
 let roleUpgrader = require('role.upgrader');
 let roleBuilder = require('role.builder');
@@ -11,9 +11,9 @@ module.exports.loop = function () {
 
     // Define the maximum numbers of creeps for each role
     const harvesters_max = 3;
-    const upgraders_max = 5;
+    const upgraders_max = 4;
     const builders_max = 4;
-    const repairers_max = 2;
+    const repairers_max = 1;
     const wallrepairers_max = 0;
 
     // Create arrays containing all roles
@@ -30,48 +30,53 @@ module.exports.loop = function () {
     //let energy = Game.spawns.Spawn1.room.energyCapacityAvailable;
     //console.log('Current available energy:', energy)
 
-    // Respawning harvesters
-    if(harvesters.length < harvesters_max) {
+    // Respawning harvesters //TODO: error catching + improve parts handling
+    if (harvesters.length < harvesters_max) {
         let newName = 'DH' + Game.time;
         console.log('Spawning new harvester:', newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE,MOVE], newName,
-            {memory: {role: 'harvester'}});
+        let parts = Game.spawns.Spawn1.partSelection(3,4,2);
+        Game.spawns['Spawn1'].spawnCreep(parts, newName,
+            {memory: {role: 'harvester', working: false}});
     }
-    
+
     // Respawning upgraders
-    if(upgraders.length < upgraders_max) {
+    if (upgraders.length < upgraders_max) {
         let newName = 'AU' + Game.time;
         console.log('Spawning new upgrader:', newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], newName,
+        let parts = Game.spawns.Spawn1.partSelection(4,4,3);
+        Game.spawns['Spawn1'].spawnCreep(parts, newName,
             {memory: {role: 'upgrader'}});
     }
-    
+
     // Respawning builders
-    if(builders.length < builders_max) {
+    if (builders.length < builders_max) {
         let newName = 'DB' + Game.time;
         console.log('Spawning new builder:', newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], newName,
+        let parts = Game.spawns.Spawn1.partSelection(4,4,3);
+        Game.spawns['Spawn1'].spawnCreep(parts, newName,
             {memory: {role: 'builder'}});
     }
 
     // Respawning repairers
-    if(repairers.length < repairers_max) {
+    if (repairers.length < repairers_max) {
         let newName = 'IR' + Game.time;
         console.log('Spawning new repairer:', newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE], newName,
+        let parts = Game.spawns.Spawn1.partSelection(3,2,1);
+        Game.spawns['Spawn1'].spawnCreep(parts, newName,
             {memory: {role: 'repairer'}});
     }
 
-    // Respawning repairers
-    if(wallrepairers.length < wallrepairers_max) {
+    // Respawning wall repairers
+    if (wallrepairers.length < wallrepairers_max) {
         let newName = 'WR' + Game.time;
         console.log('Spawning new wall repairer:', newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE], newName,
+        let parts = Game.spawns.Spawn1.partSelection(4,2,2);
+        Game.spawns['Spawn1'].spawnCreep(parts, newName,
             {memory: {role: 'wallrepairer'}});
     }
 
-    // Spawning the creeps
-    if(Game.spawns['Spawn1'].spawning) {
+    // Showing what role is being spawned
+    if (Game.spawns['Spawn1'].spawning) {
         let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
             '🛠️' + spawningCreep.memory.role,
@@ -80,36 +85,31 @@ module.exports.loop = function () {
             {align: 'left', opacity: 0.8});
     }
 
-    // Calling each role
-    for(let name in Game.creeps) {
+    // Call each role
+    for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        for(let i = 0; i < roles.length; i++) {
-            if(creep.memory.role == roles_str[i]) {
+        for (let i = 0; i < roles.length; i++) {
+            if (creep.memory.role == roles_str[i]) {
                 roles[i].run(creep)
             }
         }
     }
 
-    // Detect and shoot enemies
-    let towers = _.filter(Game.structures, object => object.structureType == STRUCTURE_TOWER);
+    // Create an array containing every tower in the room
+    var hostiles = Game.rooms['W36S38'].find(FIND_HOSTILE_CREEPS)
+    if (hostiles.length > 0) {
+        var towers = Game.rooms['W36S38'].find(
+            FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+        towers.forEach(tower => tower.attack(hostiles[0]));
+        console.log("ALERT - WE ARE BEING ATTACKED - TARGET:", hostiles[0])
+    }
     
-    for (let i = 0; i < towers.length; i++) {
-        var target = towers[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (target) {
-            towers[i].attack(target);
-            console.log("ALERT - WE ARE BEING ATTACKED - ALERT")
-            console.log('Target:', target);
-        }   
-    };
-
     // Clearing the memory of non-existing creeps
-    for(let name in Memory.creeps) {
-        if(!Game.creeps[name]) {
+    for (let name in Memory.creeps) {
+        if (!Game.creeps[name]) {
             delete Memory.creeps[name];
-            console.log('Clearing non-existing creeps in memory:', name);
+            console.log('Clearing non-existing creep in memory:', name);
         }
     }
 
-    //console.log('Current CPU usage:', (Math.round(Game.cpu.getUsed() * 100) / 1000));
-    
 }
