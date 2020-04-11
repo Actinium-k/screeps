e68604ff58065ef1ed596490362b21c50dcbecaa
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-11 13:36:25 +0000

    Fixed targets list

--- role.repairer.js
+++ role.repairer.js
@@ -6,34 +6,36 @@ let roleRepairer = {
 
     run: function(creep) {
 
-        if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.repairing = false;
         }
-        if(!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+        if (!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
             creep.memory.repairing = true;
         }
 
         // Repair the first structure in the list
-        if(creep.memory.repairing) {
+        if (creep.memory.repairing) {
             // Creating an array of all structures that need repairs
             let targets = creep.room.find(FIND_STRUCTURES, {
-                filter: object => object.hits < (object.hitsMax)
-                && object.structureType != STRUCTURE_WALL
+                filter: s => s.hits < (s.hitsMax)
+                && s.structureType != STRUCTURE_WALL
             });
 
             // Sorting the structures from least decayed to most decayed
             targets = targets.sort(function(a,b) {
-                return b.hits - a.hits
+                return (a.hits*100/a.hitsMax) - (b.hits*100/b.hitsMax)
             });
             
-            // DEBUG: logging all targets
-            /*for(let i = 0; i < targets.length; i++) {
-                console.log(targets[i].hits, i)
+            /*// DEBUG: logging all targets
+            for(let i = 0; i < (targets.length - 80); i++) {
+                console.log((targets[i].hits * 100 / targets[i].hitsMax) + "%", i)
+                //console.log(targets[i].hits, i)
             }*/
 
-            if(targets.length) {
-                if(creep.repair(targets[targets.length - 1]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[targets.length - 1], {reusePath: 3}, {visualizePathStyle: {stroke: '#ff0000'}});
+            if (targets.length) {
+                if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0],
+                        {reusePath: 5, visualizePathStyle: {stroke: '#ff0000'}});
                 }
             }
             // If there's nothing to repair, switch to builder
@@ -43,7 +45,7 @@ let roleRepairer = {
         }
         // Refill from energy source
         else {
-            creep.refill();
+            creep.refill(useSource=true);
         }
     }
 };
