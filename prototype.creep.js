ca61c8a3f47a3a247c19f6fa84395bcd8a384220
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-11 13:37:38 +0000

    Added parameters for refill(), new function moveToRoom()

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,29 +1,79 @@
 module.exports = function() {
 
-    // Refill from energy source
-    Creep.prototype.refill = function() {
-        // Create an array of non-empty tombstones and active sources
-        let sources = (this.room.find(FIND_TOMBSTONES, {
-            filter: (t) => t.store[RESOURCE_ENERGY] > 0
-        })).concat(this.room.find(FIND_SOURCES_ACTIVE));
-        
-        // Find the closest source of energy
-        let target = this.pos.findClosestByPath(sources);
-        
-        // If the target is a tombstone, withdraw from it
-        if(target instanceof Tombstone && target.store[RESOURCE_ENERGY] > 0) {
-            if(this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
+    // Refill from tombstone or energy source
+    Creep.prototype.refill = function(useSource, useContainer, useStorage) {
+
+        // Select creeps with a low life expectancy
+        if (this.ticksToLive < this.store.getCapacity()) {
+
+            // Calculate the number of body parts in each creep
+            var numberOfWorkParts = 0;
+            this.body.forEach(function(part) {
+                if (part.type == WORK) {
+                    numberOfWorkParts++;
+                }
+            })
+
+            // Kill the creep if they won't be able to refill entirely
+            if (this.ticksToLive < (this.store.getFreeCapacity() / numberOfWorkParts * 2 + 10)) {
+                console.log('Suiciding creep', this.name)
+                this.suicide();
             }
         }
-        // Else, the target is a source so we harvest it
-        else if(this.harvest(target) == ERR_NOT_IN_RANGE) {
-                return this.moveTo(target, {reusePath: 3, visualizePathStyle: {stroke: '#ffaa00'}});
+
+        // If useSource is true, harvest energy from sources
+        if (useSource) {
+            // Create an array of active sources
+            let sources = this.room.find(FIND_SOURCES_ACTIVE);
+            
+            // Find the closest source of energy
+            let target = this.pos.findClosestByPath(sources);
+            
+            // Harvest from the closest active source
+            if (this.harvest(target) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 5, visualizePathStyle: {stroke: '#ffaa00'}});
+            }
         }
+
+        // If useContainer is true, withdraw energy from containers
+        if (useContainer) {
+            let containers = this.room.find(FIND_MY_STRUCTURES, {
+                filter: s => s.structureType == STRUCTURE_CONTAINER
+            });
+
+            let target = this.pod.findClosestByPath(containers);
+
+            if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 5, visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+
+        // If useStorage is true, withdraw energy from storage
+        if (useStorage) {
+            let storage = this.room.find(FIND_MY_STRUCTURES, {
+                filter: s => s.structureType == STRUCTURE_STORAGE
+            });
+
+            let target = this.pod.findClosestByPath(storage);
+
+            if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                return this.moveTo(target, {reusePath: 5, visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+
+    };
+
+    Creep.prototype.moveToRoom = function(target_room) {
+
+        // Move to the specified room
+        var exit = this.room.findExitTo(target_room);
+        return this.moveTo(this.pos.findClosestByRange(exit),
+            {reusePath: 8, visualizePathStyle: {stroke: '#000000'}});
+    
     };
 
     /*// Debug function
     Creep.prototype.sayHello = function() {
         return this.say('Hello!');
     };*/
-}
+};
