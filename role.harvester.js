26bf03270c43a86d62896d0a8aa6888b5ce376d5
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-12 14:45:05 +0000

    Minor changes

--- role.harvester.js
+++ role.harvester.js
@@ -2,26 +2,25 @@
 require('prototype.creep')();
 
 let roleHarvester = {
-
     run: function(creep) {
 
-        // If the creep is working and has less than 50 energy, stop working
-        if (creep.memory.working == true && creep.store.getUsedCapacity() < 50) {
+        // If the creep is working and has less than 50 energy, switch to non-working state
+        if (creep.memory.working && creep.store.getUsedCapacity() < 50) {
             creep.memory.working = false;
         }
         // Once the creep is full, switch to working state
-        else if (creep.memory.working == false && creep.store.getUsedCapacity() == creep.store.getCapacity()) {
+        else if (!creep.memory.working && creep.store.getUsedCapacity() == creep.store.getCapacity()) {
             creep.memory.working = true;
         }
 
         // Find an energy source to harvest from
-        if(creep.memory.working == false) {
-            creep.refill();
+        if (!creep.memory.working) {
+            creep.refill(useSource=true);
         }
 
         // Refill existing structures with energy
-        else if(creep.memory.working) {
-            var targets = creep.room.find(FIND_STRUCTURES, {
+        else if (creep.memory.working) {
+            var targets = creep.room.find(FIND_MY_STRUCTURES, {
                 filter: (s) => {
                     return (s.structureType == STRUCTURE_EXTENSION ||
                         s.structureType == STRUCTURE_SPAWN ||
@@ -30,23 +29,23 @@ let roleHarvester = {
                 }
             });
 
-            // Find closest target
-            var target = creep.pos.findClosestByPath(targets);
+            // If the targets exist in the room
+            if (targets.length) {
 
-            if(targets.length > 0) {
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {reusePath: 3}, {visualizePathStyle: {stroke: '#ffffff'}});
+                // Find closest target
+                var target = creep.pos.findClosestByPath(targets);
+
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {reusePath: 5}, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
             // Move to a parking area if the structures are full
-            else if((creep.transfer(target, RESOURCE_ENERGY) == ERR_FULL)
+            else if ((creep.transfer(target, RESOURCE_ENERGY) == ERR_FULL)
                     || (creep.transfer(target, RESOURCE_ENERGY) == ERR_INVALID_TARGET)) {
                 creep.moveTo(Game.flags['parking_area_W36S38'])
             }
         }
-        else {
-            creep.say('ERROR')
-        }
+        
     }
 };
 
