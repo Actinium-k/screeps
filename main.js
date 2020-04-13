75f05235a4bfceac2c5b1c29e999e9ae35bde807
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-13 15:53:05 +0000

    Added support for the claimer role

--- main.js
+++ main.js
@@ -22,13 +22,21 @@ module.exports.loop = function () {
     for (const spawn_index in Game.spawns) {
 
         // Define the maximum numbers of creeps for each role
-        const harvesters_max = 2;
-        const upgraders_max = 3;
-        const builders_max = 4;
-        const repairers_max = 2;
+        const harvesters_max = 3;
+        const upgraders_max = 4;
+        const builders_max = 3;
+        const repairers_max = 1;
         const wallrepairers_max = 1;
         const miners_max = 1;
-        const remoteharvesters_max = 4;
+        const remoteharvesters_max = 10;
+        const claimers_max = 1;
+
+        // Describing all exits in the room
+        const exits = Game.map.describeExits(Game.spawns[spawn_index].room.name);
+        var exit_list = []
+        for (exit in exits) {
+            exit_list.push(exits[exit])
+        }
 
         // Create arrays containing every creep of each role
         let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
@@ -38,6 +46,7 @@ module.exports.loop = function () {
         let wallrepairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'wallrepairer');
         let miners = _.filter(Game.creeps, (creep) => creep.memory.role == 'miner');
         let remoteharvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'remoteharvester');
+        let claimers = _.filter(Game.creeps, (creep) => creep.memory.role == 'claimer');
         
         // Run each role
         for (let name in Game.creeps) {
@@ -48,7 +57,7 @@ module.exports.loop = function () {
                 }
             }
         }
-        
+
         // Respawning harvesters
         if (harvesters.length < harvesters_max) {
             Game.spawns[spawn_index].respawnCreep(spawn_index, 'harvester', 'HA', [false, undefined],
@@ -58,25 +67,25 @@ module.exports.loop = function () {
         // Respawning upgraders
         if (upgraders.length < upgraders_max) {
             Game.spawns[spawn_index].respawnCreep(spawn_index, 'upgrader', 'UP', [undefined, undefined],
-            {n_move: 3, n_work: 3, n_carry: 3})
+            {n_move: 4, n_work: 4, n_carry: 4})
         }
 
         // Respawning builders
         if (builders.length < builders_max) {
             Game.spawns[spawn_index].respawnCreep(spawn_index, 'builder', 'BD', [undefined, undefined],
-            {n_move: 3, n_work: 3, n_carry: 3})
+            {n_move: 4, n_work: 4, n_carry: 4})
         }
 
         // Respawning repairers
         if (repairers.length < repairers_max) {
             Game.spawns[spawn_index].respawnCreep(spawn_index, 'repairer', 'RP', [undefined, undefined],
-            {n_move: 3, n_work: 3, n_carry: 3})
+            {n_move: 4, n_work: 4, n_carry: 4})
         }
 
         // Respawning wall repairers
         if (wallrepairers.length < wallrepairers_max) {
             Game.spawns[spawn_index].respawnCreep(spawn_index, 'wallrepairer', 'WR', [undefined, undefined],
-            {n_move: 3, n_work: 3, n_carry: 3})
+            {n_move: 4, n_work: 4, n_carry: 4})
         }
 
         // Respawning miners
@@ -87,8 +96,19 @@ module.exports.loop = function () {
         
         // Respawning remote harvesters
         if (remoteharvesters.length < remoteharvesters_max) {
-            Game.spawns[spawn_index].respawnCreep(spawn_index, 'remoteharvester', 'RH', [false, 'W35S38'],
+            // Select a random exit from exit_list
+            var rand = Math.random();
+            rand *= exit_list.length;
+            rand = Math.floor(rand);
+            // Spawn a remote harvester whose target is a random room nearby
+            Game.spawns[spawn_index].respawnCreep(spawn_index, 'remoteharvester', 'RH', [false, exit_list[rand]],
             {n_move: 6, n_work: 3, n_carry: 3})
+        }
+        
+        // Respawning claimers
+        if (claimers.length < claimers_max) {
+            Game.spawns[spawn_index].respawnCreep(spawn_index, 'claimer', 'CL', [false, 'W35S38'],
+            {n_move: 2, n_claim: 2})
         }
 
         // Defend the room against enemies
