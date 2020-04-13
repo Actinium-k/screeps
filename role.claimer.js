53f907c53d1987e2f73cb4db5cba36f21d0ae159
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-13 15:53:22 +0000

    Overhauled the entire role

--- role.claimer.js
+++ role.claimer.js
@@ -1,20 +1,16 @@
 let roleClaimer = {
-
     run: function(creep) {
-        let target = Game.flags['claim_W35S38']
-        if (creep.room == Game.rooms['W35S38']) {
-            if (creep.room.controller) {
-                if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller, {reusePath: 8}, {visualizePathStyle: {stroke: '#dddddd'}});
-                }
-                else if (creep.claimController(creep.room.controller) == ERR_GCL_NOT_ENOUGH) {
-                    console.log('GCL not enough to claim this room');
-                }
-            }
+
+        if (creep.room.name != creep.memory.target) {
+            creep.moveToRoom(creep.memory.target);
         }
-        else {
-            creep.moveTo(target, {reusePath: 8}, {visualizePathStyle: {stroke: '#dddddd'}});
+        else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller, {reusePath: 8}, {visualizePathStyle: {stroke: '#dddddd'}});
         }
+        else if (creep.reserveController(creep.room.controller) == ERR_GCL_NOT_ENOUGH) {
+            console.log('GCL not enough to claim this room');
+        }
+        
     }
 };
 
