cabfafbff78e2ea5eb4f845b77ee2162905a97f9
o99uoaCw <57313059+Actinium-k@users.noreply.github.com>
2020-04-14 10:21:50 +0000

    Now fixes rampart up until a defined threshold

--- role.repairer.js
+++ role.repairer.js
@@ -3,7 +3,6 @@ require('prototype.creep')();
 let roleBuilder = require('role.builder');
 
 let roleRepairer = {
-
     run: function(creep) {
 
         if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
@@ -12,26 +11,24 @@ let roleRepairer = {
         if (!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
             creep.memory.repairing = true;
         }
-
+        creep.say('here')
         // Repair the first structure in the list
         if (creep.memory.repairing) {
             // Creating an array of all structures that need repairs
             let targets = creep.room.find(FIND_STRUCTURES, {
-                filter: s => s.hits < (s.hitsMax)
+                filter: s => s.hits < (s.hitsMax/2)
                 && s.structureType != STRUCTURE_WALL
+                && (s.structureType != STRUCTURE_RAMPART < 1000000)
             });
 
             // Sorting the structures from least decayed to most decayed
-            targets = targets.sort(function(a,b) {
-                return (a.hits*100/a.hitsMax) - (b.hits*100/b.hitsMax)
-            });
+            targets = targets.sort();
             
-            /*// DEBUG: logging all targets
-            for(let i = 0; i < (targets.length - 80); i++) {
-                console.log((targets[i].hits * 100 / targets[i].hitsMax) + "%", i)
-                //console.log(targets[i].hits, i)
+            // DEBUG: logging all targets
+            /*for(let i = 0; i < (targets.length - 80); i++) {
+                console.log((targets[i].hits * 100 / targets[i].hitsMax) + "%", i, targets[i].structureType)
             }*/
-
+            
             if (targets.length) {
                 if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0],
@@ -45,8 +42,9 @@ let roleRepairer = {
         }
         // Refill from energy source
         else {
-            creep.refill(useSource=true);
+            creep.refill(true, true);
         }
+        
     }
 };
 
